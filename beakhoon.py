4be9f727287f1cfacf17c82889f3c2ad17a01ae1
hyemi1129 <kimhyemi1243@gmail.com>
2023-10-14 21:40:21 +0900

    test

--- beakhoon.py
+++ beakhoon.py
@@ -9,3 +9,12 @@ elif n<m:
 elif n==m:
     print('==') """
 
+""" n = int(input())
+if n%400==0 or n%4==0 and n%100!=0:
+    print(1)
+else:
+    print(0) """
+
+n = int(input())
+for i in range(1, 10):
+    print(n, '*', i, '=', n*i)
