b1a88c7551fcf0efbd328641be3c581414e4fc58
hyemi1129 <kimhyemi1243@gmail.com>
2023-10-18 19:27:12 +0900

    short study

--- beakhoon.py
+++ beakhoon.py
@@ -15,6 +15,19 @@ if n%400==0 or n%4==0 and n%100!=0:
 else:
     print(0) """
 
-n = int(input())
+""" n = int(input())
 for i in range(1, 10):
-    print(n, '*', i, '=', n*i)
+    print(n, '*', i, '=', n*i) """
+
+""" t = int(input())
+
+for _ in range(t):
+    a, b = map(int, input().split())
+    print(a+b) """
+
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    sum += i
+
+print(sum)
